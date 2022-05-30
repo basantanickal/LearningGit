36490ccb0b9bea586851389fd8cae21080d67246
Basanta Sharma <basantas1@verifone.com>
2022-05-30 20:39:39 +0530

    Second commit

--- MyFirstJavaProgram.java
+++ MyFirstJavaProgram.java
@@ -1,5 +1,7 @@
 class MyFirstJavaProgram {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println("Changes for second commit");
+        System.out.println("Some more changes for second commit");
     }
 }
