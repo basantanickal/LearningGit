4a4bcf497bad5e8b08710c0649409a3d7f385370
Basanta Sharma <basantas1@verifone.com>
2022-05-31 19:52:09 +0530

    Added new feature

---
+++ Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 5;
+        int answer = num1 * num2;
+        System.out.println(answer);
+    }
+}
